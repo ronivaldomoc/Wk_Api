1e2f718563a9be78925a621b4039feaa707b4a8e
Roni <ronivaldomoc@gmail.com>
2022-10-27 22:02:11 -0300

    Add files via upload

---
+++ server/view/uPrincipal.pas
@@ -0,0 +1,97 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.AppEvnts, Vcl.StdCtrls, IdHTTPWebBrokerBridge, Web.HTTPApp,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  FireDAC.Comp.DataSet;
+
+type
+  TfServidor = class(TForm)
+    ButtonStart: TButton;
+    ButtonStop: TButton;
+    EditPort: TEdit;
+    Label1: TLabel;
+    ApplicationEvents1: TApplicationEvents;
+    ButtonOpenBrowser: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure ApplicationEvents1Idle(Sender: TObject; var Done: Boolean);
+    procedure ButtonStartClick(Sender: TObject);
+    procedure ButtonStopClick(Sender: TObject);
+    procedure ButtonOpenBrowserClick(Sender: TObject);
+  private
+    FServer: TIdHTTPWebBrokerBridge;
+    procedure StartServer;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fServidor: TfServidor;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  WinApi.Windows, Winapi.ShellApi, Datasnap.DSSession;
+
+procedure TfServidor.ApplicationEvents1Idle(Sender: TObject; var Done: Boolean);
+begin
+  ButtonStart.Enabled := not FServer.Active;
+  ButtonStop.Enabled := FServer.Active;
+  EditPort.Enabled := not FServer.Active;
+end;
+
+procedure TfServidor.ButtonOpenBrowserClick(Sender: TObject);
+var
+  LURL: string;
+begin
+  StartServer;
+  LURL := Format('http://localhost:%s', [EditPort.Text]);
+  ShellExecute(0,
+        nil,
+        PChar(LURL), nil, nil, SW_SHOWNOACTIVATE);
+end;
+
+procedure TfServidor.ButtonStartClick(Sender: TObject);
+begin
+  StartServer;
+end;
+
+procedure TerminateThreads;
+begin
+  if TDSSessionManager.Instance <> nil then
+    TDSSessionManager.Instance.TerminateAllSessions;
+end;
+
+procedure TfServidor.ButtonStopClick(Sender: TObject);
+begin
+  TerminateThreads;
+  FServer.Active := False;
+  FServer.Bindings.Clear;
+end;
+
+procedure TfServidor.FormCreate(Sender: TObject);
+begin
+  FServer := TIdHTTPWebBrokerBridge.Create(Self);
+end;
+
+procedure TfServidor.StartServer;
+begin
+  if not FServer.Active then
+  begin
+    FServer.Bindings.Clear;
+    FServer.DefaultPort := StrToInt(EditPort.Text);
+    FServer.Active := True;
+  end;
+end;
+
+end.
